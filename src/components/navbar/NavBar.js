ca5bbfa765771ab2d07f336f186f970e169418e0
Gabriel Galvez <gabriel.g.galvez1985@gmail.com>
2023-01-06 21:30:32 -0300

    secondCommit

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -8,22 +8,39 @@ import CartWidget from '../cartWidget/CartWidget.js';
 //Core
 
 /*Logica*/
-const NavBar = (props) =>{//funcion constructora
+const NavBar = (props) => {//funcion constructora
 
     const fecha = new Date()
     //retorno que se va a renderizar
-    return(
+    return (
         <header className='container bg-dark bg-gradient text-primary'>
             <h1 className='d-flex justify-content-center'>Bienvenidos a AppGalvez </h1>
             <p>Fecha actual: {fecha.getDate()}</p>
-            <ul className='navbar navbar-expand-lg navbar-dark bg-dark'>
-                <a href="" className="text-decoration-none">Home</a>
-                <a href="" className="text-decoration-none">Producto</a>
-                <a href="" className="text-decoration-none">Categoria</a>
-                <a href="" className="text-decoration-none">Nosotros</a>
-                <a href="" className="text-decoration-none">Carrito</a>
-            </ul>
-            <CartWidget cantidad = "10"/>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#a">Home</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#a">Producto</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#a">Categoria</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#a">Nosotros</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#a">Carrito</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <CartWidget cantidad="10" />
         </header>
     );
 
